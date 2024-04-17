97bd1753524b105c36faacb30ca12e95978c415b
NguyenCongHaiNam <h1n4m.work@gmail.com>
2024-04-17 23:39:17 +0700

    them chart va table

--- FE/obj/Debug/net8.0/FE.AssemblyInfo.cs
+++ FE/obj/Debug/net8.0/FE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e7d22e31fcbb16f5516bf7487101dea326f21c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbab1213f681e57b6b128f57ffba81b2a3ef44d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
