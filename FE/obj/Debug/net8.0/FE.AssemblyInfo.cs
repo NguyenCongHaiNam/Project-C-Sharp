f174880470783e0638a3fb89e5dabb07fcf12284
NguyenCongHaiNam <h1n4m.work@gmail.com>
2024-04-24 09:02:25 +0700

    sua docker-compose

--- FE/obj/Debug/net8.0/FE.AssemblyInfo.cs
+++ FE/obj/Debug/net8.0/FE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23699925c0f0f5f930ba413e6a5bf73301cd6e4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4992b4a228accefe5d9ed5177f6babead588f4ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("FE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
